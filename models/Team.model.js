e87df70a1d95c6463c429d341168fc341212e564
Juansecode <juan.sebastianri28@gmail.com>
2022-11-12 14:50:22 -0500

    Made all schemas of the project and implemented the models

---
+++ models/Team.model.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose');
+
+const teamSchema = new Schema(
+    {
+        members: [{
+            type: String,
+            required: true,
+            trim: true
+        }],
+        area: {
+            type: String,
+            required: true
+        },
+        isCompleteTask: {
+            type: Boolean,
+            default: false
+        },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+module.exports = model('Team', teamSchema);
---
+++ models/User.model.js
@@ -0,0 +1,27 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        mail: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        firstName: String,
+        lastName: String,
+        teams: [{
+            type: Schema.Types.ObjectId,
+            ref: 'team'
+        }]
+    },
+    {
+        versionKey: false
+    }
+);
+
+module.exports = model('User', userSchema);
