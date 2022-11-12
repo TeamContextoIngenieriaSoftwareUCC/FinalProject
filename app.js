aa2135bf1d3c90d160f9e3e20e90580431e5e38f
Juansecode <juan.sebastianri28@gmail.com>
2022-11-12 14:54:07 -0500

    Implement the app to listen the request

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const {PORT, app} = require('./config/server.js');
+const userRoutes = require('./routes/user.routes.js');
+
+app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server in port ${PORT}`);
