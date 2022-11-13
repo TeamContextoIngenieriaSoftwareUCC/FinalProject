31f81f7666d15f525012690ddf1b2f886f994d87
Juansecode <juan.sebastianri28@gmail.com>
2022-11-12 19:19:17 -0500

    Implement the routes on our app

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const {PORT, app} = require('./config/server.js');
+const isLogged = require('./middleware/isLogged.js');
 const userRoutes = require('./routes/user.routes.js');
+const teamRoutes = require('./routes/team.routes.js');
 
 app.use('/users', userRoutes);
+
+app.use(isLogged);
+app.use('/teams', teamRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server in port ${PORT}`);
